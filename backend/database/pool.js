b06028a927734c9a09359552c8b563fa63e869f0
ajaymrk95 <ajaymrk21@gmail.com>
2026-02-28 16:26:06 +0530

    ResetDefaultDB

--- backend/database/pool.js
+++ backend/database/pool.js
@@ -19,7 +19,7 @@ export const dbConnect = async()=>
     try {
         
         
-        const {rows} = await pool.query("DROP TABLE urls")
+        const {rows} = await pool.query("SELECT * from urls")
         console.log(rows)
         
 
